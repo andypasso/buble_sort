e9ce9ec0106580befe16ccb9c6caf142f24016b4
andynarf <untalandy@gmail.com>
2019-08-29 16:49:20 -0300

    linter fixes

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,25 +1,25 @@
-def bubble_sort (array)
-    j=0
-    while j<array.length-j
-        i=0
+# frozen_string_literal: true
 
-        while i < array.length-1-i
-            if array[i] >= array[i+1]
-                temp = array [i] 
-                array [i] = array [i+1]
-                array [i+1] = temp
-                
-            end
-            i=i+1
-        end 
-        j=j+1
+def bubble_sort(array)
+  j = 0
+  while j < array.length - j
+    i = 0
+
+    while i < array.length - 1 - i
+      if array[i] >= array[i + 1]
+        temp = array [i]
+        array [i] = array [i + 1]
+        array [i + 1] = temp
+
+      end
+      i += 1
     end
-    return array
-
+    j += 1
+  end
+  array
 end
 
+array = [8, 5, 6, 3, 4, 7, 8, 9]
 
-array=[8,5,6,3,4,7,8,9]
-
-bubble_sort (array)
+bubble_sort array
 puts array
