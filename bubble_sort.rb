b355988812a180824d7edf629ede36ca8c7fe98c
andynarf <untalandy@gmail.com>
2019-08-29 13:37:35 -0300

    first step

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,7 +1,9 @@
 def bubble_sort (array)
-    i=0
-    for j in (1...array.length)
-        while i < array.length-1 
+    j=0
+    while j<array.length-j
+        i=0
+
+        while i < array.length-1-i
             if array[i] >= array[i+1]
                 temp = array [i] 
                 array [i] = array [i+1]
@@ -10,13 +12,14 @@ def bubble_sort (array)
             end
             i=i+1
         end 
+        j=j+1
     end
     return array
-
 
 end
 
 
 array=[8,5,6,3,4,7,8,9]
+
 bubble_sort (array)
 puts array
